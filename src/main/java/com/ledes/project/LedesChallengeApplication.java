6db3df655e33ffeaa006ef066c6ba57b7039263a
Murilo Fernandes <murilofsilva11@gmail.com>
2024-07-25 22:43:01 -0400

    initial commit

---
+++ src/main/java/com/ledes/project/LedesChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.ledes.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedesChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LedesChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ledes/project/LedesChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ledes.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedesChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
